273019c93b3dc9becad6bf636250fffc211ab14f
EC2 Default User <ec2-user@ip-172-31-93-189.ec2.internal>
2023-08-02 19:49:39 +0900

    ブログ投稿の作成を行いました

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*
 Route::get('/', function () { return view('welcome'); });
 
-Route::get('/posts', [PostController::class, 'index']);   
+Route::get('/posts', [PostController::class, 'index']);  
+*/
 
 Route::get('/',[PostController::class,'index']);
 
+Route::get('/posts/create',[PostController::class,'create']);
+
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::post('/posts', [PostController::class, 'store']);
